865071ba4633d9a5436df93392eac9728012335b
Zoltan Jakubecz <jakuzo@gmail.com>
2019-12-03 13:18:38 +0100

    Starter board with two random coordinates

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,29 @@
+const board = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+]
+
+function randomStartCoordinates(){
+    let x = Math.floor(Math.random() * 15) + 1;
+    let y = Math.floor(Math.random() * 15) + 1;
+    return [x,y];
+}
+
+function starterBoard(){
+    let coordinates = randomStartCoordinates();
+
+    while (coordinates[0] == coordinates[1]){
+        coordinates = randomStartCoordinates();
+    }
+    console.log(coordinates);
+    const cells = document.querySelectorAll('.cell');
+    cells[coordinates[0]].textContent = '16';
+    cells[coordinates[1]].textContent = '4';
+
+}
+
 function main() {
     const container = document.querySelector('.container');
     for(let row = 0; row < 4; row++){
@@ -7,6 +33,7 @@ function main() {
             container.appendChild(newCell)
         }
     }
-}
+    starterBoard();
+    }
 
 main();
