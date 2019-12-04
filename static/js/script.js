322b91969491a0b1ef1fa4af0edc32c0948684aa
zsoltion-cc <55063936+zsoltion-cc@users.noreply.github.com>
2019-12-04 09:18:20 +0100

    Restore file

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,25 @@
-let board = [
+const board = [
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0]
 ];
 
-function RandomCoordinates() {
-    let x = Math.floor(Math.random() * 4) + 1;
-    let y = Math.floor(Math.random() * 4) + 1;
+function randomStartCoordinates(){
+    let x = Math.floor(Math.random() * 3) + 1;
+    let y = Math.floor(Math.random() * 3) + 1;
     return [x,y];
 }
 
-function starterBoard() {
-    let coordinates1 = RandomCoordinates();
-    let coordinates2 = RandomCoordinates();
-    while(coordinates1 == coordinates2) {
-        coordinates1 = RandomCoordinates();
-        coordinates2 = RandomCoordinates();
-    }
+function drawBoard(){
     const cells = document.querySelectorAll('.cell');
-    cells[coordinates1[0][1]].textContent = '2';
-    cells[coordinates2[0][1]].textContent = '4';
+    let arrBoard = board.flat();
+    for(let i=0; i<cells.length; i++){
+        if(arrBoard[i]){
+            cells[i].textContent = arrBoard[i];
+        }
+    }
+
 }
 
 function randomStartNumbers(){
